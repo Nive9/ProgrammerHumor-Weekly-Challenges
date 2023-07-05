dbd6ca0a83f6ccc6fff876529e9b52b06c9e5a65
Nive9 <29470670+Nive9@users.noreply.github.com>
2023-07-05 20:59:36 +0200

    Update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 
 fun fizzBuzz(r:IntProgression) {
     for (i in r) {
-        when (i) {
-            in 3..3 -> {
-                println("Fizz")
-            }
-            in 5 ..5 -> {
-                println("Buzz")
-            }
-            else -> {
-                println("$i")
-            }
+        if (i in 3 ..10 step 3 && i in 5..10 step 5) {
+            println("FizzBuzz")
+        }
+        else if (i in 3..i step 3) {
+            println("Fizz ($i)")
+        }
+        else if (i in 5 ..i step 5){
+            println("Buzz ($i)")
+        }
+        else {
+            println("$i")
         }
     }
 }
