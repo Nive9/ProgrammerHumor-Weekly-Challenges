5cd526d3501405355184a150dff8138f3b43ef6e
Nive9 <29470670+Nive9@users.noreply.github.com>
2023-07-06 14:40:59 +0200

    Update

--- Readme.md
+++ Readme.md
@@ -2,13 +2,13 @@
 * **written in Kotlin**
 * **To execute code: [Kotlin Playground](https://play.kotlinlang.org/)**
 ## First Challenge: FizzBuzz
-### Easy Mode
+### Easy
 [Source](src/main/kotlin/FizzBuzz-Easy.kt)
 
 [Run in Browser / Playground](https://pl.kotl.in/zRNddM1tY)
 
 
-### Intermediate Mode (at least it works...?)
+### Intermediate
 [Source](src/main/kotlin/FizzBuzz-Intermediate.kt)
 
-[Run in Browser / Playground](https://pl.kotl.in/QZzcWq1Wb)
+[Run in Browser / Playground](https://pl.kotl.in/oBPelxTcK)
--- src/main/kotlin/FizzBuzz-Intermediate.kt
+++ src/main/kotlin/FizzBuzz-Intermediate.kt
@@ -1,81 +1,35 @@
 // Run in browser/ Playground
-// https://pl.kotl.in/QZzcWq1Wb
+// https://pl.kotl.in/oBPelxTcK
 
 fun fizzBuzzIntermediate(q:IntProgression) {
-    val fizz = "Fizz"
-    val fizzNo = 3
-
-    val buzz = "Buzz"
-    val buzzNo = 5
-
-    val rizz = "Rizz"
-    val rizzNo = 7
-
-    val jazz = "Jazz"
-    val jazzNo = 11
-
-    val dizz = "Dizz"
-    val dizzNo = 120
-
     for (i in q) {
-//        Abomination starts here:
-//        Fizz+
-        if (i in fizzNo ..i step fizzNo && i in buzzNo..i step buzzNo) {
-            println(fizz + buzz)
-        }
-        else if (i in fizzNo ..i step fizzNo && i in rizzNo..i step rizzNo) {
-            println(fizz + rizz)
-        }
-        else if (i in fizzNo ..i step fizzNo && i in jazzNo..i step jazzNo) {
-            println(fizz + jazz)
-        }
-        else if (i in fizzNo ..i step fizzNo && i in dizzNo..i step dizzNo) {
-            println(fizz + dizz)
-        }
+        val fizz = i in 3..i step 3
+        val buzz = i in 5..i step 5
+        val rizz = i in 7..i step 7
+        val jazz = i in 11..i step 11
+        val dizz = 120 % i == 0
 
-        //        Buzz+
-        if (i in buzzNo ..i step buzzNo && i in rizzNo..i step rizzNo) {
-            println(buzz + rizz)
-        }
-        else if (i in buzzNo ..i step buzzNo && i in jazzNo..i step jazzNo) {
-            println(buzz + jazz)
-        }
-        else if (i in fizzNo ..i step fizzNo && i in dizzNo..i step dizzNo) {
-            println(buzz + dizz)
-        }
-
-        //        Rizz+
-        else if (i in rizzNo ..i step rizzNo && i in jazzNo..i step jazzNo) {
-            println(rizz + jazz)
-        }
-        else if (i in rizzNo ..i step rizzNo && i in dizzNo..i step dizzNo) {
-            println(rizz + dizz)
-        }
-
-        //        Jazz+
-        else if (i in jazzNo ..i step jazzNo && i in dizzNo..i step dizzNo) {
-            println(jazz + dizz)
-        }
-
-//        Abomination ends here?
-        else if (i in fizzNo..i step fizzNo) {
-            println(fizz)
-        }
-        else if (i in buzzNo ..i step buzzNo){
-            println(buzz)
-        }
-        else if (i in rizzNo ..i step rizzNo){
-            println(rizz)
-        }
-        else if (i in jazzNo ..i step jazzNo){
-            println(jazz)
-        }
-        else if (i in dizzNo ..i step dizzNo){
-            println(dizz)
+        if (!fizz && !buzz && !rizz && !jazz && !dizz) {
+            print(i)
         }
         else {
-            println("$i")
+            if(fizz) {
+                print("Fizz")
+            }
+            if(buzz) {
+                print("Buzz")
+            }
+            if(rizz) {
+                print("Rizz")
+            }
+            if(jazz) {
+                print("Jazz")
+            }
+            if(dizz) {
+                print("Dizz")
+            }
         }
+        println("")
     }
 }
 
